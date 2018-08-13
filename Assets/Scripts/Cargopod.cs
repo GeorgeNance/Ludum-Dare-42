6d76bcab785e9d6eed26dc559122d84b940ee4df
George Nance <thatbuddhadude@gmail.com>
2018-08-13 12:41:32 -0700

    Fixes

---
+++ Assets/Scripts/Cargopod.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cargopod : MonoBehaviour,Interactable {
+
+
+    public GameObject WinScreen;
+
+    public string HoverText()
+    {
+        return "Take Cargo pod";
+    }
+
+    public void Interact(GameObject player)
+    {
+        WinScreen.SetActive(true);
+        player.GetComponent<CamerController>().enabled = false;
+        player.GetComponent<PlayerMovement>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
